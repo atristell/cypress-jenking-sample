39bf5439bbcb22050e1455dd3a43ee5ae8be21af
atristell <agarciat@eprinsa.es>
2018-12-24 15:53:56 +0100

    Update Tests

--- tareas/cypress/integration/google/google.js
+++ tareas/cypress/integration/google/google.js
@@ -2,7 +2,7 @@ describe('Google Search', function() {
   it('Cypress.json', function() {
     cy
     .log(Cypress.config())
-    .visit('search/q=cypress')
+    .visit('search?q=cypress')
     expect(1).to.eq(1) 
   })
 })
